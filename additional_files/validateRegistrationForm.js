c776151714a8319e0ea036f07e2d1f832a983338
fengchej <fengchej@mcmaster.ca>
2019-10-13 21:05:16 -0400

    validate registration form

---
+++ additional_files/validateRegistrationForm.js
@@ -0,0 +1,94 @@
+function validateForm() {
+	var x = false;
+	//calling all functions instead of using && to show all invalid inputs
+	// if using &&, it stops validating the rest of the conditions which will skip the style changing for the invalid input
+	x = validateFirstName();
+	x = validateLastName();
+	x = validateEmail();
+	x = validatePassword();
+	return x;
+}
+
+function validateFirstName() {
+	var form = document.RegistrationForm;
+	//this element is the error message for validating the first name
+	element = document.getElementById("error_msg_fn");
+	//only letters
+	var regex = /^[a-z]+$/ ; 
+	if (form.firstname.value != "") {
+		//check the input whether it satisfies the regular expression
+		if (regex.test(String(form.firstname.value).toLowerCase())) {
+			//hide error message
+			form.firstname.style.outline = "none";
+			element.style.display = "none";
+			return true;
+		} else {
+			element.innerHTML = "Field can only include letters.";
+		}
+	}
+	//show error message
+	form.firstname.style.outline = "1px solid red";
+	element.style.display = "block";
+	return false;
+}
+
+function validateLastName() {
+	var form = document.RegistrationForm;
+	//this element is the error message for validating last name
+	element = document.getElementById("error_msg_ln");
+	//only letters
+	var regex = /^[a-z]+$/ ; 
+	if (form.lastname.value != "") {
+		//check the input whether it satisfies the regular expression
+		if (regex.test(String(form.lastname.value).toLowerCase())) {
+			//hide error message
+			form.lastname.style.outline = "none";
+			element.style.display = "none";
+			return true;
+		} else {
+			element.innerHTML = "Field can only include letters.";
+		}
+	}
+	//show error message
+	form.lastname.style.outline = "1px solid red";
+	element.style.display = "block";
+	return false;
+}
+
+function validateEmail() {
+	var form = document.RegistrationForm;
+	//this element is the error message for validating email address
+	element = document.getElementById("error_msg_email");
+	if (form.email.value != "") {
+		//regular expression for email that doesn't include unicodes
+		var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		//check the input whether it satisfies the regular expression for email defined above
+		if (regex.test(String(form.email.value).toLowerCase())) {
+			//hide error message
+			form.email.style.outline = "none";
+			element.style.display = "none";
+			return true;
+		}
+	}
+	//show error message
+	form.email.style.outline = "1px solid red";
+	element.style.display = "block";
+	return false;
+}
+
+function validatePassword() {
+	var form = document.RegistrationForm;
+	//this element is the error message for validating password
+	element = document.getElementById("error_msg_psw");
+	//password just need to be at least 8 characters long
+	if (form.usrpsw.value != "" && form.usrpsw.value.length >= 8) {
+		//hide error message
+		form.usrpsw.style.outline = "none";
+		element.style.display = "none";
+		return true;
+	}
+	//show error message
+	form.usrpsw.style.outline = "1px solid red";
+	element.style.display = "block";
+	return false;
+}
